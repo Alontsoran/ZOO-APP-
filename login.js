e34a70c9835b7004e58f16438c4585d07a5dcd83
AlonTsoran <alontsoranschool@gmail.com>
2024-02-18 11:41:15 +0200

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,9 @@ function loginAsVisitor(visitorName) {
   // תממשו את הלוגיקה של בחירת אורח שנכנס לגן החיות
   // שמרו את האורח שבחרתם, בלוקל סטורג' כך שבכל העמודים נדע מי האורח הנוכחי
 }
-
+window.addEventListener("load", () => {
+  // Render the product cards inside #placeholder
+});
 let visitorsForView = [...visitors];
 const dialog = document.querySelector("#visitor-dialog");
 
@@ -21,4 +23,16 @@ const getvisitorHTMLCard = (visitor) => {
   wrapper.innerHTML = template;
   wrapper.addEventListener("click", () => handlevisitorClick(visitor));
   return wrapper;
+};
+//נדרש עיבוד לטובת שמירה בJS + USER
+const getCloseModalHTMLButton = () => {
+  const closeButton = document.createElement("button");
+  closeButton.innerText = " Close modal";
+  closeButton.addEventListener("click", () => dialog.close());
+  return closeButton;
+};
+const handlevisitorClick = (visitor) => {
+  dialog.innerHTML = "";
+  dialog.append(getCloseModalHTMLButton(), getProductHTMLCard(product));
+  dialog.showModal();
 };
