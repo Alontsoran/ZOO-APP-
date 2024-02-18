43a8a94286fa0fa741be374765d2c1fa23335868
AlonTsoran <alontsoranschool@gmail.com>
2024-02-18 11:26:31 +0200

    Update login.js

--- login.js
+++ login.js
@@ -3,19 +3,19 @@ function loginAsVisitor(visitorName) {
   // שמרו את האורח שבחרתם, בלוקל סטורג' כך שבכל העמודים נדע מי האורח הנוכחי
 }
 
-const getProductHTMLCard = (product) => {
+const getvisitorHTMLCard = (visitor) => {
   const template = `
       <div class="card" style="min-height: 360px;" >
-        <img class="card-img-top" src="${product.thumbImage}" alt="${product.name}"/>
+        <img class="card-img-top" src="${visitor.thumbImage}" alt="${visitor.name}"/>
         <div class="card-body">
-          <p class="card-text">${product.name}</p>
-          <p class="card-text">${product.price}</p>
+          <p class="card-text">${visitor.name}</p>
+          <p class="card-text">${visitor.price}</p>
         </div>
       </div>`;
-      
-      const wrapper = document.createElement("div");
-      wrapper.className = "product-card";
-      wrapper.innerHTML = template;
-      wrapper.addEventListener("click", () => handleProductClick(product));
-      return wrapper;
-    };
+
+  const wrapper = document.createElement("div");
+  wrapper.className = "product-card";
+  wrapper.innerHTML = template;
+  wrapper.addEventListener("click", () => handleProductClick(product));
+  return wrapper;
+};
