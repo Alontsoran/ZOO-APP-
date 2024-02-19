1a8548233d4f989056cbffd3f8bfcb023309add3
zvimeir <131192591+zvimeir@users.noreply.github.com>
2024-02-19 14:48:03 +0200

    Update zoo.js
    סינון בסיסי

--- zoo.js
+++ zoo.js
@@ -21,3 +21,28 @@ function setFilter(filterKey, filterValue) {
   // ודאו כי אתם שומרים את הפילטרים שהיוזר בחר בלוקל סטורג׳ וטוענים אותם בהתאם
   // רנדרו רק את החיות שעומדות בתנאים של הפילטרים
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  renderAvailableAnimals()
+  animalInZOO = BasicFilter()
+});
+
+function BasicFilter(){
+  const animals = JSON.parse(localStorage.getItem("animals")) || []; 
+  let animalInZOO = animals.filter((animal) => animal.in_cage ===1)
+  return animalInZOO
+}
+
+//קוד כפול!!! צריך לבדוק האם ניתן להעביר את הפונקציה הזו לmain מפה ומlogin
+function getvisitorHTMLCard(animal) {
+  const template = `
+    <div id ="cardss" class="card" style="width:300px; margin: 10px; ">
+      <img class="card-img-top" src=${animal.photo} alt="תמונת ${animal.name}">
+      <div class="card-body">
+        <h5 class="card-title">${animal.name}</h5>
+        <p class="card-text">coins: ${animal.coins}</p>
+      </div>
+    </div>
+  `;
+  return template;
+}
