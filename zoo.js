c6086711bfb8e2836dcff72d40ae99bf280c5892
zvimeir <131192591+zvimeir@users.noreply.github.com>
2024-02-20 12:36:36 +0200

    קוד חלקי של הצגת הכרטיסיות

--- zoo.js
+++ zoo.js
@@ -1,8 +1,15 @@
-function renderAvailableAnimals() {
+function renderAvailableAnimals(animalInZOO) {
   // ממשו את הלוגיקה שמרנדרת את החיות לתוך הדיב עם האיידי animal-cards
   // וודאו שאתם מציגים אך ורק את החיות שעומדות בפילטורים הנוכחיים
   // במידה ואין פילטרים מסומנים, הציגו את כל החיות
+  const animals = AdvancedFilter(animalInZOO);
+  const animalsContainer =
+    document.getElementById("animal-cards");
+    animalsContainer.innerHTML = "";
+  const animalsHTML = animals.map(getvisitorHTMLCard).join("");
+  animalsContainer.innerHTML = animalsHTML;
 }
+
 function visitAnimal(animalName) {
   // ממשו את הלוגיקה של מעבר לעמוד חיה עבור החיה הספציפית שנבחרה
   // שמרו בלוקל סטורג' את החיה שנבחרה, כך שבעמוד החיה נוכל לשלוף אותה מהסטורג' ולהציגה בהתאם
@@ -22,13 +29,14 @@ function setFilter(filterKey, filterValue) {
   // רנדרו רק את החיות שעומדות בתנאים של הפילטרים
 }
 
+
+
 document.addEventListener("DOMContentLoaded", (event) => {
   // סינון ראשוני
   animalInZOO = BasicFilter();
 
   // שליחה לסינוןוהצגה
-  //afretFlret = setFilter(filterKey, filterValue)
-  //renderAvailableAnimals(afretFlret);
+  renderAvailableAnimals(animalInZOO);
 
   // קבלת אלמנט הטופס
   const filterForm = document.getElementById('filrerForm'); 
@@ -77,13 +85,33 @@ function BasicFilter(){
 //קוד כפול!!! צריך לבדוק האם ניתן להעביר את הפונקציה הזו לmain מפה ומlogin
 function getvisitorHTMLCard(animal) {
   const template = `
-    <div id ="cardss" class="card" style="width:300px; margin: 10px; ">
+    <div id="cardss" class="card" style="width:300px; margin: 10px;">
       <img class="card-img-top" src=${animal.photo} alt="תמונת ${animal.name}">
       <div class="card-body">
         <h5 class="card-title">${animal.name}</h5>
-        <p class="card-text">coins: ${animal.coins}</p>
+        <p class="card-text">diet: ${animal.isPredator ? 'Predator' : 'vegetarian'}</p>
+        <p class="card-text">Habitat: ${animal.habitat}</p>
       </div>
     </div>
   `;
   return template;
+}
+
+
+//פונקציה לפילטור
+function AdvancedFilter(animals){
+  const savedFilterSettings = JSON.parse(localStorage.getItem('filterSettings'));
+  if (!savedFilterSettings) {
+    return animals; 
+  }
+  
+  const filteredAnimals = animals.filter(animal => {
+    return (savedFilterSettings.isPredator === 'all' || animal.isPredator.toString() === savedFilterSettings.isPredator) &&
+           (savedFilterSettings.habitat === 'all' || animal.habitat === savedFilterSettings.habitat) &&
+           (!savedFilterSettings.weight || animal.weight >= parseFloat(savedFilterSettings.weight)) &&
+           (!savedFilterSettings.height || animal.height >= parseFloat(savedFilterSettings.height)) &&
+           (savedFilterSettings.color === 'all' || animal.color === savedFilterSettings.color);
+  });
+  
+  return filteredAnimals;
 }
