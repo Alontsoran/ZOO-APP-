81701566c022c7ff4a0a64926da323de4bfd7db6
zvimeir <131192591+zvimeir@users.noreply.github.com>
2024-02-21 22:28:02 +0200

    דף עם דיילוג

--- animal.js
+++ animal.js
@@ -33,7 +33,7 @@ function feedAnimal(animal , visitor) {
   }
   else {
     if (animal.isPredator) {
-      visitorGotEaten(visitor);
+      visitorGotEaten(animal ,visitor);
     }
     else {
       animalEscaped(animal);
@@ -41,14 +41,17 @@ function feedAnimal(animal , visitor) {
   }
 }
 
-function visitorGotEaten(visitor) {
+function visitorGotEaten(animal, visitor) {
   // ממשו את הלוגיקה של חיה שטורפת אורח
   visitor.alive =0;
   localStorage.setItem('visitors', JSON.stringify(visitors));
 
-  // כאן צריך להכניס דילוג
-  // כאן צריך לחזור לדף ההתחברות
-
+  const text =  `
+  <div name="${visitor.name}+name" style="width:300px; margin: 10px;">
+  <h5 class="card-title">${visitor.name} was devoured by the ${animal.name}</h5>
+  <img class="card-img-top" src="${visitor.photo}" alt="Picture of ${visitor.name}">`; 
+  // להכניס דילוג
+  openDialog(text);
 }
 
 function animalEscaped(animal) {
@@ -56,6 +59,23 @@ function animalEscaped(animal) {
   animal.in_cage = 0;
   localStorage.setItem('animals', JSON.stringify(animals));
 
+  const text =  `
+  <div name="${animal.name}+name" style="width:300px; margin: 10px;">
+  <h5 class="card-title">The ${animal.name} escaped from the zoo!!</h5>
+  <img class="card-img-top" id="dialogImg" src="${animal.photo}" alt="Picture of ${animal.name}" style="width: 290px;">`; 
+  // להכניס דילוג
+  openDialog(text);
+
+}
+
+function openDialog(text) {
+  var dialog = document.getElementById("myDialog");
+  dialog.showModal();
+  document.getElementById("displayElementId").innerHTML = text;
+  document.getElementById("Logout_from_user").addEventListener("click", function() {
+    dialog.close();
+    window.location.href = '/';
+  });
 }
 
 
@@ -63,7 +83,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   // מביא נתונים ומרנדר את הדף
   const animal = renderRelatedAnimals();
   renderAnimal(animal);
-  
+
   // מביא את הנתונים של המשתמש המחובר
   const visitor = getVisitor();
 
@@ -77,9 +97,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
 // מביא את המשתמש
 function getVisitor() {
   const visitorName = localStorage.getItem("selectedVisitor");
-  const visitor = visitors.find((visitor) => visitor.name === visitorName)// אם רוצים לחפור אפשר להוציא את זה לפונציה נפרדת ולמנוע שכפול קוד
+  const visitor = visitors.find((visitor) => visitor.name === visitorName);
   return visitor;
 }
 
 let visitors = JSON.parse(localStorage.getItem("visitors")) || [];
 let animals = JSON.parse(localStorage.getItem("animals")) || []; 
+
