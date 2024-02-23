418ac56db3482a0395315a375e3e95d5eef69b7c
AlonTsoran <alontsoranschool@gmail.com>
2024-02-23 17:47:21 +0200

    עדכונים

--- dashboard.js
+++ dashboard.js
@@ -7,3 +7,39 @@ function showFeededAnimals() {
 function showFavoriteAnimal() {
   //ממשו את הלוגיקה שמציגה את החיה שהאורח ביקר הכי הרבה פעמים אצלה
 }
+window.addEventListener("DOMContentLoaded", (event) => {
+  coins_in_grph();
+  showVisitedAnimals();
+  showFavoriteAnimal();
+  // Call other functions as needed
+});
+function coins_in_grph() {
+  var labels = [];
+  var coins = [];
+  visitors = getvisitorlist();
+  visitors.forEach((visitor) => {
+    labels.push(visitor.name);
+    coins.push(visitor.coins);
+  });
+  const ctx = document.getElementById("USercoins").getContext("2d");
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "",
+          data: coins,
+          backgroundColor: ["blue"],
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
