d9d1c5bc36836b6046ba025a29df1f48a47d8a23
zvimeir <131192591+zvimeir@users.noreply.github.com>
2024-02-23 12:13:59 +0200

    פתירה של באג הכניסה לzoo

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-//מאזין לDIV של החיות באת לחיצה מבצע מעבר
 visitor = localStorage.getItem("selectedVisitor");
-if (visitor === "" || visitor === undefined){
-    alert("You need to be logged in first!!");
-}
-else {
-    const zoo = document.getElementById(`zooDiv`);
-    zoo.addEventListener('click', () => {
+const zoo = document.getElementById(`zooDiv`);
+zoo.addEventListener('click', () => {
+    if (visitor === "undefined" || visitor === null || visitor === "" || visitor === undefined) {
+        alert("You need to be logged in first!!");
+    } else {
         window.location.href = './zoo.html';
-    });
-}
+    }
+});
+
+
 
 const login_ = document.getElementById(`loginDiv`);
 login_.addEventListener('click', () => {
