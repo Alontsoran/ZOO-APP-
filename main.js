005854f9a463913a876bdb80fff94e50e8abf6cf
zvimeir <131192591+zvimeir@users.noreply.github.com>
2024-02-24 09:08:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,18 +12,21 @@ function generateDataset() {
       coins: 50,
       photo: "https://i.ibb.co/7SpTTF5/johnsmith.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "Emily Johnson",
       coins: 50,
       photo: "https://i.ibb.co/cDxD96W/Amanda-Wilson.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "Michael Williams",
       coins: 50,
       photo: "https://i.ibb.co/Y8ZXKRy/Michael-Williams.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "Jessica Brown",
@@ -31,18 +34,21 @@ function generateDataset() {
       photo:
         "https://i.ibb.co/sVzYhDq/DALL-E-2024-02-18-20-52-56-Create-a-LEGO-figure-profile-picture-of-a-character-named-Jessica-Brown-T.webp", //
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "Christopher Jones",
       coins: 50,
       photo: "https://i.ibb.co/z8f2f04/Christopher-Jones.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "Ashley Davis",
       coins: 50,
       photo: "https://i.ibb.co/z818Lc1/Ashley-Davis.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "Matthew Miller",
@@ -50,72 +56,84 @@ function generateDataset() {
       photo:
         "https://i.ibb.co/BZVcVYK/DALL-E-2024-02-18-20-52-52-Create-a-LEGO-figure-profile-picture-of-a-character-named-Jessica-Brown-T.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "AmandaWilson",
       coins: 50,
       photo: "https://i.ibb.co/cDxD96W/Amanda-Wilson.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "DavidMoore",
       coins: 50,
       photo: "https://i.ibb.co/HFnzCPn/David-Moore.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "SarahTaylor",
       coins: 50,
       photo: "https://i.ibb.co/3S191GB/Sarah-Taylor.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "JamesAnderson",
       coins: 50,
       photo: "https://i.ibb.co/BtTBbJF/James-Anderson.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "JenniferThomas",
       coins: 50,
       photo: "https://i.ibb.co/dJkLK0T/Jennifer-Thomas.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "Robert Jackson",
       coins: 50,
       photo: "https://i.ibb.co/njHxBrg/Robert-Jackson.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "Elizabeth White",
       coins: 50,
       photo: "https://i.ibb.co/n1m5rJq/Elizabeth-White.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "DanielHarris",
       coins: 50,
       photo: "https://i.ibb.co/XSyGjP4/Daniel-Harris.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "MelissaMartin",
       coins: 50,
       photo: "https://i.ibb.co/Z6XRqbm/Melissa-Martin.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "WilliamThompson",
       coins: 50,
       photo: "https://i.ibb.co/4Rv99VW/William-Thompson.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "LindaGarcia",
       coins: 50,
       photo: "https://i.ibb.co/WfPjM86/Linda-Garcia.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "JosephMartinez",
@@ -123,6 +141,7 @@ function generateDataset() {
       photo:
         "https://i.ibb.co/CbNXJxY/DALL-E-2024-02-18-20-53-02-Create-a-LEGO-figure-profile-picture-of-a-character-named-Michael-William.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
     {
       name: "KarenRobinson",
@@ -130,9 +149,10 @@ function generateDataset() {
       photo:
         "https://i.ibb.co/ZYB8Vbg/DALL-E-2024-02-18-20-53-05-Create-a-LEGO-figure-profile-picture-of-a-character-named-Emily-Johnson-T.webp",
       alive: 1,
+      Documentation: animals_visit,
     },
   ];
-  let animals = [
+  let animals = [ 
     {
       name: "Lion",
       isPredator: true,
@@ -224,6 +244,55 @@ function generateDataset() {
       image: "https://i.ibb.co/DKZYs55/Cheetah.webp",
     },
   ];
+
+  let animals_visit = [ //יצירת ערך שמתעד את הפעילות של המשתמש, כל משתמש מקבל אחד
+    {
+      name: "Lion",
+      visits: 0,
+      feeding: 0,
+    },
+    {
+      name: "Elephant",
+      visits: 0,
+      feeding: 0,
+    },
+    {
+      name: "Giraffe",
+      visits: 0,
+      feeding: 0,
+    },
+    {
+      name: "Tiger",
+      visits: 0,
+      feeding: 0,
+    },
+    {
+      name: "Monkey",
+      visits: 0,
+      feeding: 0,
+    },
+    {
+      name: "Kangaroo",
+      visits: 0,
+      feeding: 0,
+    },
+    {
+      name: "Penguin",
+      visits: 0,
+      feeding: 0,
+    },
+    {
+      name: "Zebra",
+      visits: 0,
+      feeding: 0,
+    },
+    {
+      name: "Cheetah",
+      visits: 0,
+      feeding: 0,
+    },
+  ];
+
 
   if (localStorage.getItem("visitors")) {
     visitors = JSON.parse(localStorage.getItem("visitors"));
