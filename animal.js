d95ab1f6564f62591b1eaf76a94b431dfc98d039
zvimeir <131192591+zvimeir@users.noreply.github.com>
2024-02-24 22:34:26 +0200

    Update animal.js

--- animal.js
+++ animal.js
@@ -1,19 +1,15 @@
 function renderAnimal(animal) {
   //הציגו את החיה שאליה עברתם מעמוד גן החיות ששמורה בלוקל סטורג'
   // רנדרו את פרטי החיה לתוך האלמנטים המתאימים בהתאם לשדה הספציפי
-
-  // עדכון כל האלמנטים ב-HTML עם המידע המתאים
-  document.getElementById("name").textContent = animal.name;
-  document.getElementById("weight").textContent = animal.weight + " kg";
-  document.getElementById("height").textContent = animal.height + " cm";
-  document.getElementById("color").textContent = animal.color;
-  document.getElementById("habitat").textContent = animal.habitat;
-  document.getElementById("isPredator").textContent = animal.isPredator
-    ? "Predator"
-    : "Herbivore";
-  document.getElementById(
-    "image"
-  ).innerHTML = `<img src="${animal.image}" alt="${animal.name}" style="width:280px; margin: 10px;   max-width: 90%;" />`;
+  
+      // עדכון כל האלמנטים ב-HTML עם המידע המתאים
+  document.getElementById('name').textContent = animal.name;
+  document.getElementById('weight').textContent = animal.weight + ' kg';
+  document.getElementById('height').textContent = animal.height + ' cm';
+  document.getElementById('color').textContent = animal.color;
+  document.getElementById('habitat').textContent = animal.habitat;
+  document.getElementById('isPredator').textContent = animal.isPredator ? 'Predator' : 'Herbivore';
+  document.getElementById('image').innerHTML = `<img src="${animal.image}" alt="${animal.name}" style="width:280px; margin: 10px;   max-width: 90%;" />`;
 }
 
 function renderRelatedAnimals() {
@@ -25,25 +21,25 @@ function renderRelatedAnimals() {
   return animal;
 }
 
-function feedAnimal(animal, visitor) {
+function feedAnimal(animal , visitor) {
   // ממשו את הלוגיקה של האכלת חיה
   // במידה ואין מספיק מטבעות, טפלו בהתאם להנחיות במטלה
-  if (parseInt(visitor.coins) >= 2) {
-    visitor.coins -= 2;
+  if (parseInt(visitor.coins) >= 2){
+    visitor.coins-=2;
 
-    const animal_ = visitor.Documentation.find(
-      (animal_) => animal_.name === animal.name
-    ); //הולך לחיה המתאימה
-    animal_.feeding += 1; // מוסיף האכלה
+    const animal_ = visitor.Documentation.find(animal_ => animal_.name === animal.name); //הולך לחיה המתאימה
+    animal_.feeding +=1; // מוסיף האכלה
 
     alert("Thanks for feeding me!");
 
-    localStorage.setItem("visitors", JSON.stringify(visitors));
+    localStorage.setItem('visitors', JSON.stringify(visitors));
     window.location.reload();
-  } else {
+  }
+  else {
     if (animal.isPredator) {
-      visitorGotEaten(animal, visitor);
-    } else {
+      visitorGotEaten(animal ,visitor);
+    }
+    else {
       animalEscaped(animal);
     }
   }
@@ -51,14 +47,14 @@ function feedAnimal(animal, visitor) {
 
 function visitorGotEaten(animal, visitor) {
   // ממשו את הלוגיקה של חיה שטורפת אורח
-  visitor.alive = 0;
-  localStorage.setItem("visitors", JSON.stringify(visitors));
+  visitor.alive =0;
+  localStorage.setItem('visitors', JSON.stringify(visitors));
   localStorage.setItem("selectedVisitor", "");
 
-  const text = `
+  const text =  `
   <div name="${visitor.name}+name" style="width:300px; margin: 10px;">
   <h5 class="card-title">${visitor.name} was devoured by the ${animal.name}</h5>
-  <img class="card-img-top" src="./photos/backgrounds/lego_lion_eating_meat.jpeg" alt="Picture of a lego lion eating a visitor" style="width: 290px;">`;
+  <img class="card-img-top" src="./photos/backgrounds/lego_lion_eating_meat.jpeg" alt="Picture of a lego lion eating a visitor" style="width: 290px;">`; 
   // להכניס דילוג
   openDialog(text);
 }
@@ -66,15 +62,16 @@ function visitorGotEaten(animal, visitor) {
 function animalEscaped(animal) {
   //ממשו את הלוגיקה של חיה שבורחת מגן החיות
   animal.in_cage = 0;
-  localStorage.setItem("animals", JSON.stringify(animals));
+  localStorage.setItem('animals', JSON.stringify(animals));
   localStorage.setItem("TheChosenAnimal", "");
 
-  const text = `
+  const text =  `
   <div name="${animal.name}+name" style="width:300px; margin: 10px;">
   <h5 class="card-title">The ${animal.name} escaped from the zoo!!</h5>
-  <img class="card-img-top" id="dialogImg" src="./photos/backgrounds/lego_vegetarian_animals_escape.jpeg" alt="Picture of animals escape zoo" style="width: 290px;">`;
+  <img class="card-img-top" id="dialogImg" src="./photos/backgrounds/lego_vegetarian_animals_escape.jpeg" alt="Picture of " style="width: 290px;">`; 
   // להכניס דילוג
   openDialog(text);
+
 }
 
 //הרצת דיילוג
@@ -82,28 +79,27 @@ function openDialog(text) {
   var dialog = document.getElementById("myDialog");
   dialog.showModal();
   document.getElementById("displayElementId").innerHTML = text;
-  document
-    .getElementById("Logout_from_user")
-    .addEventListener("click", function () {
-      dialog.close();
-      window.location.href = "/";
-    });
+  document.getElementById("Logout_from_user").addEventListener("click", function() {
+    dialog.close();
+    window.location.href = '/';
+  });
 }
+
 
 document.addEventListener("DOMContentLoaded", (event) => {
   // מביא נתונים ומרנדר את הדף
   const animal = renderRelatedAnimals();
   renderAnimal(animal);
-
+  
   //יוצר את הכרטיסיות למטה
-  renderAvailableAnimals(BasicFilter());
+  renderAvailableAnimals(BasicFilter())
 
   // מביא את הנתונים של המשתמש המחובר
   const visitor = getVisitor();
 
   // מוסיף האזנה לאירוע לחיצה על הכפתור
-  const feedButton = document.getElementById("feed-animal");
-  feedButton.addEventListener("click", function () {
+  const feedButton = document.getElementById('feed-animal');
+  feedButton.addEventListener('click', function() {
     feedAnimal(animal, visitor);
   });
 });
@@ -116,12 +112,12 @@ function getVisitor() {
 }
 
 let visitors = JSON.parse(localStorage.getItem("visitors")) || [];
-let animals = JSON.parse(localStorage.getItem("animals")) || [];
+let animals = JSON.parse(localStorage.getItem("animals")) || []; 
 
 //יוצר את הבר למטה
-function BasicFilter() {
-  let animalInZOO = animals.filter((animal) => animal.in_cage == 1);
-  return animalInZOO;
+function BasicFilter(){
+  let animalInZOO = animals.filter((animal) => animal.in_cage == 1)
+  return animalInZOO
 }
 
 function renderAvailableAnimals(animalsInzoo) {
@@ -131,10 +127,10 @@ function renderAvailableAnimals(animalsInzoo) {
   const animalsHTML = animals.map(getvisitorHTMLCard).join("");
   animalsContainer.innerHTML = animalsHTML;
 
-  animals.forEach((animal) => {
+  animals.forEach(animal => {
     const card = document.getElementById(`card-${animal.name}`);
     if (card) {
-      card.addEventListener("click", () => {
+      card.addEventListener('click', () => {
         visitAnimal(animal.name);
       });
     }
@@ -143,17 +139,11 @@ function renderAvailableAnimals(animalsInzoo) {
 
 function getvisitorHTMLCard(animal) {
   const template = `
-    <div id="card-${
-      animal.name
-    }" class="cardss" style="width:200px; margin: 10px;">
-      <img class="card-img-top" src=${animal.image} alt="תמונת ${
-    animal.name
-  }" style="width:180px; margin: 10px;">
+    <div id="card-${animal.name}" class="cardss" style="width:200px; margin: 10px;">
+      <img class="card-img-top" src=${animal.image} alt="תמונת ${animal.name}" style="width:180px; margin: 10px;">
       <div class="card-body">
         <h3 class="card-title">${animal.name}</h3>
-        <p class="card-text">diet: ${
-          animal.isPredator ? "Predator" : "vegetarian"
-        }</p>
+        <p class="card-text">diet: ${animal.isPredator ? 'Predator' : 'vegetarian'}</p>
         <p class="card-text">Habitat: ${animal.habitat}</p>
       </div>
     </div>
@@ -162,16 +152,14 @@ function getvisitorHTMLCard(animal) {
 }
 
 function visitAnimal(animalName) {
-  localStorage.setItem("TheChosenAnimal", JSON.stringify(animalName));
-  window.location.href = "./animal.html";
+  localStorage.setItem('TheChosenAnimal', JSON.stringify(animalName));
+  window.location.href = './animal.html';
 }
 
 //סינון לפי סביבת מחיה
 function FilterByLivingEnvironment(animalsInzoo) {
   const AnimoalName = JSON.parse(localStorage.getItem("TheChosenAnimal"));
   const animal = animalsInzoo.find((animal) => animal.name === AnimoalName);
-  const animals = animalsInzoo.filter(
-    (animal_) => animal_.habitat === animal.habitat
-  );
+  const animals = animalsInzoo.filter((animal_) => animal_.habitat === animal.habitat);
   return animals;
 }
